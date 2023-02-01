bda166710bddb5a303c1786d9c7e17ea682abb95
Jobayer Al Madmud Tuser <jtuser112@gmail.com>
2023-02-01 08:41:00 +0600

    made function to parse json data

--- src/main/java/org/topupgateway/topup/ProcessCampaignClient.java
+++ src/main/java/org/topupgateway/topup/ProcessCampaignClient.java
@@ -5,7 +5,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class ProcessCampaignClient {
     protected ResourceBundle prop = ResourceBundle.getBundle("config");
@@ -18,16 +18,35 @@ public class ProcessCampaignClient {
         HttpRequest httpRequest = HttpRequest.newBuilder()
                 .header("accept", "application/json")
                 .uri(URI.create(prop.getString("apiUrl")))
-                .POST(HttpRequest.BodyPublishers.ofString("hello"))
+                .POST(HttpRequest.BodyPublishers.ofString(params))
                 .build();
 
         HttpResponse<String> httpResponse = campaignClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
-        System.out.println(httpResponse);
+//        System.out.println(httpResponse.body());
+        parseJson(httpResponse.body());
         return httpResponse.body();
     }
 
-    public String createAJsonString(ProcessCampaignEntity campaignEntity){
+    private String createAJsonString(ProcessCampaignEntity campaignEntity) {
         return "{" + "\"campaignLotId\":\"" + campaignEntity.campaignLotId() + "\"," + "\"referenceId\":\"" + campaignEntity.transactionId() + "\"," + "\"contactNumber\":\"" + campaignEntity.contactNumber() + "\"," + "\"balance\":\"" + campaignEntity.balance() + "\"," + "\"contactType\":\"" + campaignEntity.contactType() + "\"," + "\"contactOperator\":\"" + campaignEntity.contactOperator() + "\"" + "}";
+    }
+
+    private void parseJson(String value) {
+        Map<String, Object> map = new HashMap<>();
+        String[] pairs = value.replace("{", "").replace("}", "").split(",");
+
+        Arrays.stream(pairs)
+                .map(pair -> pair.split(":"))
+                .forEach(keyValue -> map.put(keyValue[0], keyValue[1]));
+
+        /* Split the array and put the value in map using foreach
+            for (String pair: pairs){
+                String keyValue[] = pair.split(":");
+                map.put(keyValue[0], keyValue[1]);
+            }
+         */
+
+            System.out.println(map);
     }
 
 }
